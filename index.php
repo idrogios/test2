cbe43f9f611a6f3e338136db8c56dfd48ae562ae
geomel <geomel@gmail.com>
2020-03-20 19:46:18 +0200

    A logic to show records from the database

--- index.php
+++ index.php
@@ -12,4 +12,16 @@ if ($conn->connect_error) {
 }
 $sql = "SELECT sch_id, sch_name, sch_long, sch_lat FROM schools";
 
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "School id: " . $row["sch_id"]. " - Name: " . $row["sch_name"]. " - Long: " . $row["sch_long"]. " Lat: " . $row["sch_lat"]." <br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
 ?>
